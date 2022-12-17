4ff1b8203003103fc701a3187c7f684045eb969d
Tailsthedark <100260776+Tailsthedark@users.noreply.github.com>
2022-12-18 00:00:33 +0300

    Add files via upload

---
+++ Day2part1.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+#include<fstream>
+#include<string>
+using namespace std;
+
+int main(){
+
+ifstream file;
+file.open("Day2.txt");
+if(file.fail()){
+    cout<<"Unable to open the file"<<endl;
+}
+string line;//variable that stores a line from the file given.
+char letter[2];
+int num=0;
+int sum=0;                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
+while(getline(file,line)){//Rock=1, Paper=2, Scissors=3, Win=6, Draw=3 , Loss=0  //A and X=rock, B and Y=paper, C and Z=scissors
+letter[0]=line[0];//stores the first character of the string  //ABC for player 1 XYZ for player2
+letter[1]=line[2];//stores the third character of the string 
+if(letter[0]=='A'&&letter[1]=='X'){
+num=4;
+sum+=num;
+}
+else if(letter[0]=='A'&&letter[1]=='Y'){
+num=8;
+sum+=num;
+}
+else if(letter[0]=='A'&&letter[1]=='Z'){
+num=3;
+sum+=num;
+}
+else if(letter[0]=='B'&&letter[1]=='X'){
+num=1;
+sum+=num;
+}
+else if(letter[0]=='B'&&letter[1]=='Y'){
+num=5;
+sum+=num;
+}
+else if(letter[0]=='B'&&letter[1]=='Z'){
+num=9;
+sum+=num;
+}
+else if(letter[0]=='C'&&letter[1]=='X'){
+num=7;
+sum+=num;
+}
+else if(letter[0]=='C'&&letter[1]=='Y'){
+num=2;
+sum+=num;
+}
+else if(letter[0]=='C'&&letter[1]=='Z'){
+num=6;
+sum+=num;
+}
+}
+file.close();
+cout<<sum;
+}
+
